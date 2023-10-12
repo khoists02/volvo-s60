5a2435466d6e9ba4e4019ab899dcf16475138a6c
Khoi LE <khoi.kioto@gmail.com>
2023-10-12 19:30:07 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./app.scss";
 import Sidebar from "./parts/Sidebar";
 import Navbar from "./parts/Navbar";
@@ -9,9 +9,11 @@ import { useLocation } from "react-router-dom";
 
 function App() {
   const path = useLocation();
+
+  const [showNoti, setShowNoti] = useState(false);
   return (
     <>
-      <Navbar />
+      <Navbar show={showNoti} toggle={(s) => setShowNoti(s)} />
       <div className="page-content">
         <Sidebar />
         <div className="content-wrapper">
@@ -22,6 +24,17 @@ function App() {
             </div>
           </div>
           <Footer />
+        </div>
+      </div>
+      <div className={`offcanvas offcanvas-end ${showNoti ? "show" : ""}`} id="notification">
+        <div className="offcanvas-header py-0 d-flex justify-content-between">
+          <h5 className="offcanvas-title pt-3 pb-3 pl-3">Activity</h5>
+          <button type="button" onClick={() => setShowNoti(false)} className="btn btn-light btn-sm btn-icon border-transparent rounded-pill mt-3 mb-3 mr-3">
+            <i className="ph-light ph-sm-size ph-x"></i>
+          </button>
+        </div>
+        <div className="offcanvas-body p-0">
+          
         </div>
       </div>
     </>
--- src/parts/Navbar.tsx
+++ src/parts/Navbar.tsx
@@ -3,7 +3,10 @@ import LogoIcon from "../assets/images/logo_icon.svg";
 import LogoTxtLight from "../assets/images/logo_text_light.svg";
 import { Link } from "react-router-dom";
 
-const Navbar: FC = () => {
+const Navbar: FC<{ show: boolean; toggle: (show: boolean) => void }> = ({
+  show,
+  toggle,
+}) => {
   return (
     <div className="navbar navbar-dark navbar-expand-lg navbar-static border-bottom border-bottom-white border-opacity-10">
       <div className="container-fluid">
@@ -27,7 +30,14 @@ const Navbar: FC = () => {
           </Link>
         </div>
         <div className="flex-1"></div>
-        <ul className="nav flex-row justify-content-end order-1 order-lg-2"></ul>
+        <ul className="nav flex-row justify-content-end order-1 order-lg-2">
+          <li className="nav-item">
+            <span className="navbar-nav-link navbar-nav-link-icon rounded-pill" onClick={() => toggle(!show)}>
+              <i className="ph-light ph-sm-size ph-bell"></i>
+              <span className="badge bg-yellow text-black position-absolute top-0 end-0 translate-middle-top zindex-1 rounded-pill mt-1 me-1">2</span>
+            </span>
+          </li>
+        </ul>
       </div>
     </div>
   );
